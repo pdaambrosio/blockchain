9f545c4b4e11613d7a3481aa1ac6871de4482bae
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-31 20:00:07 -0300

    feat: validation rule

--- blockChain.js
+++ blockChain.js
@@ -16,4 +16,21 @@ module.exports = class BlockChain {
     this.blocks.push(block);
     this.nextIndex++;
   }
+
+  isValid() {
+    for (let i = 1; i < this.blocks.length; i++) {
+      const currentBlock = this.blocks[i];
+      const previousBlock = this.blocks[i - 1];
+
+      if (currentBlock.hash !== currentBlock.generateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 };
