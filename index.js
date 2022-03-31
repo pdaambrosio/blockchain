d73b4dca3e2b1b537899353ac0a7520113d3086a
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-31 20:05:02 -0300

    test: function validation rule

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const blockChain = new BlockChain();
 
 blockChain.addBlock({ from: "a", to: "b", amount: 100 });
 blockChain.addBlock({ from: "c", to: "d", amount: 100 });
+
 console.log(blockChain);
+console.log(blockChain.isValid());
+
+blockChain.blocks[1].data = { from: "c", to: "d", amount: 200 };
+
+console.log(blockChain);
+console.log(blockChain.isValid());
