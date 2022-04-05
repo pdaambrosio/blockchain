be3009d6d6d16df33985dc34e191395c22ad3b19
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-04-04 22:43:02 -0300

    function mine

--- block.js
+++ block.js
@@ -1,12 +1,22 @@
 const sha256 = require("crypto-js/sha256");
 
 module.exports = class Block {
-  constructor(index = 0, previousHash = null, data = "Genesis Block") {
+  // eslint-disable-next-line prettier/prettier
+  constructor(index = 0, previousHash = null, data = "Genesis Block", _difficulty = "") {
     this.index = index;
     this.timestamp = new Date();
     this.data = data;
     this.previousHash = previousHash;
-    this.hash = this.generateHash();
+    this.nonce = 0;
+
+    this.mine(_difficulty);
+  }
+
+  mine(difficulty) {
+    do {
+      this.nonce++;
+      this.hash = this.generateHash();
+    } while (!this.hash.startsWith(difficulty));
   }
 
   generateHash() {
@@ -14,6 +24,7 @@ module.exports = class Block {
       this.index +
         this.previousHash +
         this.timestamp +
+        this.nonce +
         JSON.stringify(this.data)
     ).toString();
   }
